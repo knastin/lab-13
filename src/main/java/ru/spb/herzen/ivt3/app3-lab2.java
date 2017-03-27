eaeb95f07fafc2ca903d9c367c18808c5caa42a7
knastin <knastin@users.noreply.github.com>
2017-03-27 18:10:43 +0300

    Create app3-lab2.java

---
+++ src/main/java/ru/spb/herzen/ivt3/app3-lab2.java
@@ -0,0 +1,44 @@
+package ru.spb.herzen.three;
+
+import java.util.Random;
+
+public class MainThree extends Thread {
+
+    private Account account = new Account(1000000);
+    private Random random = new Random();
+
+    public static void main(String[] args) {
+        MainThree r = new MainThree();
+        Thread first = new Thread(r);
+        Thread second = new Thread(r);
+        first.setName("Pavel");
+        second.setName("Tatiana");
+        first.start();
+        second.start();
+    }
+    @Override
+    public void run() {
+        while (true) {
+            takeMoney(random.nextInt(100000));
+            if (account.getBalance() <= 0) {
+                System.out.println("Try to get money from empty account! You have: " + account.getBalance());
+                break;
+            }
+        }
+    }
+
+    private synchronized void takeMoney(int money) {
+        if (account.getBalance() >= money) {
+            System.out.println(Thread.currentThread().getName() + " have " + account.getBalance() + " and going to get money");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            account.getMoney(money);
+            System.out.println(Thread.currentThread().getName() + " successful getting money");
+        } else {
+            System.out.println("You try to take more then have. You have: " +	account.getBalance());
+        }
+    }
+}
