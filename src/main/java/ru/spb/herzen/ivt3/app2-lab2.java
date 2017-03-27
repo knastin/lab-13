93c7e370d853109f1a2d75fd40f70326950d697d
knastin <knastin@users.noreply.github.com>
2017-03-27 18:05:32 +0300

    Update app2-lab2.java

--- src/main/java/ru/spb/herzen/ivt3/app2-lab2.java
+++ src/main/java/ru/spb/herzen/ivt3/app2-lab2.java
@@ -1,1 +1,33 @@
+package herzen.ivt3;
 
+
+public class App
+{
+    public static void main(String... args) {
+        SyncCounterThread syncFirstThread = new SyncCounterThread();
+        SyncCounterThread syncSecondThread = new SyncCounterThread();
+        syncFirstThread.start();
+        syncSecondThread.start();
+
+        CounterThread firstThread = new CounterThread();
+        CounterThread secondThread = new CounterThread();
+        firstThread.start();
+        secondThread.start();
+    }
+}
+
+class CounterThread extends Thread {
+    public void run(){
+        for(Integer i=1; i<=100; i++){
+            System.out.println(i + " from " + this.getName());
+        }
+    }
+}
+
+class SyncCounterThread extends Thread {
+    synchronized public void run(){
+        for(Integer i=1; i<=100; i++){
+            System.out.println(i + " from " + this.getName());
+        }
+    }
+}
